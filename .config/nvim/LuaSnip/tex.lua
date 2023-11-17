88284ddf09d8258948b11cb6ca1fb961d8b14fb1
derRiesenOtter <robin.ender@icloud.com>
2023-11-18 00:52:30 +0100

    Package Manager konfiguriert, Luasnip konfiguriert

---
+++ .config/nvim/LuaSnip/tex.lua
@@ -0,0 +1,21 @@
+local ls = require("luasnip")
+local s = ls.snippet
+local t = ls.text_node
+local i = ls.insert_node
+local fmta = require("luasnip.extras.fmt").fmta
+
+return{
+
+  s("figure",
+    fmta(
+      [[
+        \begin{figure}
+          \includegraphics[width=\textwidth]{./figures/<>}
+          \caption{<>}
+        \end{figure}
+      ]],
+      { i(1), i(2)}
+    )
+  ),
+
+}
--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,3 +1,5 @@
+-- LAZY.NVIM PACKAGE MANAGER
+
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
   vim.fn.system({
@@ -10,3 +12,59 @@ if not vim.loop.fs_stat(lazypath) then
   })
 end
 vim.opt.rtp:prepend(lazypath)
+
+-- MAPLEADER
+
+vim.g.mapleader = " "
+
+-- PLUGINS
+
+require("lazy").setup({
+
+{
+	"L3MON4D3/LuaSnip",
+	-- follow latest release.
+	version = "v2.*", -- Replace <CurrentMajor> by the latest released major (first number of latest release)
+	-- install jsregexp (optional!).
+	build = "make install_jsregexp"
+},
+
+})
+
+-- TAB TO 2 SPACES
+
+-- Use spaces instead of tabs
+vim.o.expandtab = true
+-- Set the number of spaces for each tab
+vim.o.tabstop = 2
+-- Set the number of spaces to use for auto-indent
+vim.o.shiftwidth = 2
+-- Indent when you press the Tab key
+vim.o.smarttab = true
+
+-- LUASNIP
+
+-- Somewhere in your Neovim startup, e.g. init.lua
+require("luasnip").config.set_config({ -- Setting LuaSnip config
+
+  -- Enable autotriggered snippets
+  enable_autosnippets = true,
+
+  -- Use Tab (or some other key if you prefer) to trigger visual selection
+  store_selection_keys = "<Tab>",
+})
+
+vim.cmd[[
+" Expand or jump in insert mode
+imap <silent><expr> <Tab> luasnip#expand_or_jumpable() ? '<Plug>luasnip-expand-or-jump' : '<Tab>' 
+
+" Jump forward through tabstops in visual mode
+smap <silent><expr> <Tab> luasnip#jumpable(1) ? '<Plug>luasnip-jump-next' : '<Tab>'
+
+" Jump backward through snippet tabstops with Shift-Tab (for example)
+imap <silent><expr> <S-Tab> luasnip#jumpable(-1) ? '<Plug>luasnip-jump-prev' : '<S-Tab>'
+smap <silent><expr> <S-Tab> luasnip#jumpable(-1) ? '<Plug>luasnip-jump-prev' : '<S-Tab>'
+]]
+
+-- Load all snippets from the nvim/LuaSnip directory at startup
+require("luasnip.loaders.from_lua").load({paths = "~/.config/nvim/LuaSnip/"})
